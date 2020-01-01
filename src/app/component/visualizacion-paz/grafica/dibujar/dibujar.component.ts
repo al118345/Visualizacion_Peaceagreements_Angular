b680c4d7cf8c86000c84151d47bef229f76c526e
ruben perez ibañez <rubenpeib@gmail.com>
2020-01-02 00:22:45 +0100

    subimos la infomación

--- src/app/component/visualizacion-paz/grafica/dibujar/dibujar.component.ts
+++ src/app/component/visualizacion-paz/grafica/dibujar/dibujar.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Item} from '../../../../model/Factura';
 import {DatePipe} from '@angular/common';
 import {Pais} from '../../../../model/Pais';
+import {Tratados} from '../../../../model/Tratados';
 
 @Component({
   selector: 'app-dibujar',
@@ -10,10 +11,13 @@ import {Pais} from '../../../../model/Pais';
 })
 export class DibujarComponent implements OnInit {
   @Input() paises: Array<Pais>;
+  @Input() tratados: Array<Tratados>;
+
   pais: Pais;
   // lineChart
   public lineChartData: Array<any>;
   public lineChartLabels: Array<String> = [];
+  public chartColors: Array<any> = [];
   public lineChartOptions: any = {
     responsive: true
   };
@@ -31,7 +35,6 @@ export class DibujarComponent implements OnInit {
   ngOnInit() {
 
     const lenght = this.paises.length;
-    alert(lenght);
     let iterator = 0;
     let lista_colores = [];
 
@@ -40,19 +43,41 @@ export class DibujarComponent implements OnInit {
 
     let contador_array = -1;
     while (iterator < lenght) {
-      if ( lista_colores.indexOf(this.paises[iterator].supp) === -1) {
+      if (lista_colores.indexOf(this.paises[iterator].supp) === -1) {
         contador_array = contador_array + 1;
         lista_colores.push(this.paises[iterator].supp);
-        this.lineChartData.push( {data: [],  fill: false, label: this.paises[iterator].supp })
+        this.lineChartData.push({data: [], pointRadius: [], pointStyle: [] ,  fill: false, label: this.paises[iterator].supp})
       }
       this.pais = this.paises[iterator];
-      this.lineChartData[contador_array].data.push(this.pais.esperanza_vida);
+      let paz = false;
+      const tam_tratados = this.tratados.length;
+      let trat = 0;
+      while (trat < tam_tratados) {
+        if (this.pais.nombres === this.tratados[trat].x) {
+            if (this.pais.supp === this.tratados[trat].vector_pais) {
+              paz = true;
+              break;
+            }
+        }
+        trat++;
+      }
+//https://github.com/valor-software/ng2-charts/tree/master
+      if (paz) {
+        this.lineChartData[contador_array].data.push(this.pais.esperanza_vida);
+        this.lineChartData[contador_array].pointRadius.push(10);
+        this.lineChartData[contador_array].pointStyle.push('triangle');
+
+      } else {
+        this.lineChartData[contador_array].data.push(this.pais.esperanza_vida);
+        this.lineChartData[contador_array].pointRadius.push(3);
+        this.lineChartData[contador_array].pointStyle.push('circle');
+
+
+      }
       if ( contador_array === 0 ) {
-       this.lineChartLabels.push(this.pais.nombres);
+        this.lineChartLabels.push(this.pais.nombres);
       }
       iterator++;
     }
-
-
   }
 }
