aacd9c6185feb6bc380e1dca9fa26e41a931f065
ruben perez ibañez <rubenpeib@gmail.com>
2020-01-08 00:06:04 +0100

    subimos la infomación

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC4WBsbnB6nH0PQSfOIeOkJdwjZ1utN0es',
+    authDomain: 'websubastas-ac56b.firebaseapp.com',
+    databaseURL: 'https://websubastas-ac56b.firebaseio.com',
+    projectId: 'websubastas-ac56b',
+    storageBucket: 'websubastas-ac56b.appspot.com',
+    messagingSenderId: '894035453912',
+    appId: '1:894035453912:web:e97598fc030ad6047a34ad',
+    measurementId: 'G-10RM505HPD'
+  }
 };
