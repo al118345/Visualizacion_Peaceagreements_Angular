0e90bd876d4d07b98acf60956f9482823882a682
Ruben Perez Ibañez <al118345@uji.es>
2020-01-08 16:03:44 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyC4WBsbnB6nH0PQSfOIeOkJdwjZ1utN0es',
-    authDomain: 'websubastas-ac56b.firebaseapp.com',
-    databaseURL: 'https://websubastas-ac56b.firebaseio.com',
-    projectId: 'websubastas-ac56b',
-    storageBucket: 'websubastas-ac56b.appspot.com',
-    messagingSenderId: '894035453912',
-    appId: '1:894035453912:web:e97598fc030ad6047a34ad',
-    measurementId: 'G-10RM505HPD'
+   
   }
 };
